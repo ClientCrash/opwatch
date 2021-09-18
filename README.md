1d119eaf2c86ba272c4421d7c35696dd812f2355
ClientCrash <clientcrashyt@gmail.com>
2021-09-18 18:00:26 +0200

    added basic code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# opwatch
-Watch what your Minecraft operators / mods are doing. 
+# OpWatch 
+[![CodeFactor](https://www.codefactor.io/repository/github/clientcrash/opwatch/badge)](https://www.codefactor.io/repository/github/clientcrash/opwatch)
+
+  Watch what your Minecraft operators / mods are doing. 
--- src/main/java/com/github/clientcrash/opwatch/Opwatch.java
+++ src/main/java/com/github/clientcrash/opwatch/Opwatch.java
@@ -1,17 +1,18 @@
 package com.github.clientcrash.opwatch;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Opwatch extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        Bukkit.getServer().broadcastMessage("$2OPWATCH ENABLED");
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        Bukkit.getServer().broadcastMessage("$cOPWATCH DISABLED");
     }
 }
