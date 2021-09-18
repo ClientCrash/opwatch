d1093c5642226abf214f2079a0302304f18743dc
ClientCrash <clientcrashyt@gmail.com>
2021-09-19 01:55:57 +0200

    added Full Report method

--- src/main/java/com/github/clientcrash/opwatch/report/ReportCreator.java
+++ src/main/java/com/github/clientcrash/opwatch/report/ReportCreator.java
@@ -4,6 +4,7 @@ import com.github.clientcrash.opwatch.CommandExecution;
 import com.github.clientcrash.opwatch.Opwatch;
 import com.github.clientcrash.opwatch.util.CommandExecutionListUtils;
 
+import java.awt.*;
 import java.util.ArrayList;
 
 public class ReportCreator {
@@ -18,8 +19,16 @@ public class ReportCreator {
 
         return report;
     }
+    public static ArrayList<String> createFullReport(){
+        ArrayList<String> report = new ArrayList<String>();
+        ArrayList<CommandExecution> elements = Opwatch.cmsStore.cmds;
+        for(CommandExecution e : elements){
+            report.add("§e" + e.time.toString() + " " + e.author + " >> " + e.Command);
+        }
+        return report;
+    }
     private static ArrayList<String> generateReportFromExecutions(ArrayList<CommandExecution> execs){
-        ArrayList<String> report =  new ArrayList<String>();
+        ArrayList<String> report =  new ArrayList<>();
         for(CommandExecution e : execs){
             report.add("§e" + e.time.toString() + " " + e.author + " >> " + e.Command);
         }
